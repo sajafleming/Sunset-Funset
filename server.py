9abe377812fcedddd3a5e2e73b5b487b0c40d1a4
Sarah Fleming <sajafleming@gmail.com>
2016-05-18 14:56:16 -0700

    get latlong from user and return coordinates as results

--- server.py
+++ server.py
@@ -2,7 +2,7 @@
 
 from jinja2 import StrictUndefined
 from flask import Flask, render_template, redirect, request, flash, session, jsonify
-from utilities import validate_location_for_search, find_local_maxima, pick_n_best_points, find_tile_name, create_filename
+from utilities import find_tile_name, create_filename, pick_n_best_points, validate_location_for_search
 # from flask_debugtoolbar import DebugToolbarExtension
 from flask_sqlalchemy import SQLAlchemy
 from model import connect_to_db, db, LatLong
@@ -25,20 +25,34 @@ def home():
     return render_template("homepage.html")
 
 @app.route('/sunset-spots', methods=["GET"])
-def search_results():
+def find_points():
     """Search results all pretty looking and such"""
 
     # get lat/lng from URL parameters named "lat" and "lng"
-    latlong = (36.79, -117.05) # placeholder
+    # latlong = (36.79, -117.05) # placeholder
+    lat = float(request.args.get("lat"))
+    lng = float(request.args.get("lng"))
+
+    latlong = (lat, lng)
+
+    print "\n"
+    print latlong
 
     # validate that the location the user wants to search is within the US
     if validate_location_for_search(latlong):
 
-        # query db for exact coordinates of NW corner of master array
+        
         filename_of_original_latlong, n, w = find_tile_name(latlong)
-        top_left_filename = create_filename(n + 1, w - 1)
+        print filename_of_original_latlong
+        top_left_filename = create_filename(n + 1, w + 1) # want to add 1 to w here because filename represented as positive
+        print "#############################################"
+        print top_left_filename
+        top_left_db_filename = top_left_filename.split(".")[0]
+        print top_left_db_filename
 
-        exact_coordinates = LatLong.query.filter_by(filename=top_left_filename).first()
+        # TODO: add condition for if the top left tile doesn't exist
+        # query db for exact coordinates of NW corner of master array
+        exact_coordinates = LatLong.query.filter_by(filename=top_left_db_filename).first()
         exact_N_bound = exact_coordinates.n_bound
         exact_W_bound = exact_coordinates.w_bound
 
@@ -50,8 +64,10 @@ def search_results():
 
         # hella = {"1": "you hella cool"}
         # data = {"results": n_sunset_spots }
+        results = jsonify({"results": n_sunset_spots})
         
-        return jsonify(results=n_sunset_spots)
+        return results
+        # return jsonify(hella)
         # google_json = request.form.get('user-location')
         # print google_json
 
