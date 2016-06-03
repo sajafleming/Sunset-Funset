8132dfea96e0c09e71685ec73cdd416610c0266b
Sarah Fleming <sajafleming@gmail.com>
2016-06-02 23:15:59 -0700

    renamed file-renaming.py HAHA

---
+++ file_renaming.py
@@ -0,0 +1,15 @@
+"""Renaming img files"""
+
+from os import rename, listdir
+
+def rename_files(list_of_filenames):
+
+    for name in list_of_filenames:
+        new_name = name[3:-6]
+
+        # rename file
+        rename("/Users/Sarah/PROJECT/imgfiles/" + name, "/Users/Sarah/PROJECT/imgfiles/" + new_name + ".img")
+
+list_of_filenames = listdir("/Users/Sarah/PROJECT/imgfiles/")
+    
+rename_files(list_of_filenames)
